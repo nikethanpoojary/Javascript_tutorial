38b5726c08e56e237c881f7798ef14990b74a377
nikethanpoojary <nikethanpoojaryvenur@gmail.com>
2025-10-08 15:58:27 +0530

    daily updates

--- 01_basics/strings.js
+++ 01_basics/strings.js
@@ -1,6 +1,18 @@
 let strName = new String('Gagan')  // another method for string declaration
 
-console.log(strName.length);
-console.log(strName[3]);
+// console.log(strName.length);
+// console.log(strName[3]);
+
+// console.log(`${strName.toUpperCase()} is in our office`)
+// console.log(strName.charAt(2));
+// console.log(strName.indexOf('a'));
+
+let strName2 = "Vira"
+let strName3 = strName.concat(" ",strName2)
+console.log(strName3);
+// console.log(strName3.endsWith('ra'));
+// console.log(strName3.search("V"));
+console.log(strName3.slice(-7,-3));       
+console.log(strName3.substring(-4,7)); //treat -4 as 0
 
 
